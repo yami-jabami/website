60a05c347ec623f64a25c4ad0fd6ff3e2d1ab19a
UwUssimo Robinson <uwussimo@icloud.com>
2022-06-14 23:19:07 +0500

    hello world

---
+++ main.jsx
@@ -0,0 +1,15 @@
+/** @jsx h */
+
+import blog from "https://deno.land/x/blog@0.3.3/blog.tsx";
+
+await blog({
+  title: "Yami Jabami",
+  author: "Yami Jabami",
+  avatar: "./jabami.jpeg",
+  avatarClass: "rounded-full",
+  links: [
+    { title: "Email", url: "mailto:yami.jabami@icloud.com" },
+    { title: "GitHub", url: "https://github.com/yami-jabami" },
+  ],
+  background: "#f9f9f9"
+});
---
+++ posts/hello_world.md
@@ -0,0 +1,6 @@
+---
+title: Hello world!
+publish_date: 2022-05-19
+---
+
+This is my first blog post!
